ef2dd62f03fc6889b3d266b992ab209f407999b7
Christopher Kohnert <ckohnert@gmail.com>
2017-08-21 11:47:18 -0700

    Add letter m test

--- test/test_msdf.py
+++ test/test_msdf.py
@@ -65,6 +65,43 @@ def check_imagemagick():
         sys.exit(1)
 
 
+def compare(ref_path, render_path, diff_path, total_pixels, args):
+    # Use imagemagick to rasterize the reference image and compare it.
+    # We use a very fuzzy comparison to try and account for differences in aliasing introduced around edges of
+    # an image, which comes from both the down/up rezzing introduced by going through the SDF process, but also
+    # minor differences with IM's SVG rasterizer. What we're trying to catch is full on pixel mismatches that
+    # indicate a shape was mis-handled (curve shooting off to the edge, fill rule violation, etc).
+
+    p = Popen(["compare -fuzz %f%% -metric AE \"%s\" \"%s\" \"%s\"" % (args.fuzz, ref_path, render_path, diff_path)],
+              stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
+    output, err = p.communicate("")
+    passed = False
+    if p.returncode == 1:
+        # Ran successfully, but they are different. Let's parse the output and check the actual error metric.
+        match = re.match(r'^(\d+)$', err)
+        passed = False
+        if match:
+            try:
+                pixels = int(match.group(1))
+                e = float(pixels) / total_pixels * 100.0
+                if e > args.fail_threshold:
+                    msg = "Error = %.3f %% (%d)" % (e, pixels)
+                else:
+                    passed = True
+                    msg = "OK = %.3f %% (%d)" % (e, pixels)
+            except ValueError:
+                msg = "(Unknown) Error metric = %s" % err
+        else:
+            msg = "(Unknown) Error metric = %s" % err
+    elif p.returncode != 0:
+        msg = "Error comparing %s to %s [%d]: %s" % (ref_path, render_path, p.returncode, err)
+    else:
+        passed = True
+        msg = "Identical"
+
+    return (passed, msg)
+
+
 def test_svg(path, args, runner):
     (root, ext) = os.path.splitext(path)
 
@@ -92,41 +129,52 @@ def test_svg(path, args, runner):
     except OSError as er:
         return runner.add_fail(path, "Error running %s: %s" % (args.exe, er))
 
-    # Use imagemagick to rasterize the reference image and compare it.
-    # We use a very fuzzy comparison to try and account for differences in aliasing introduced around edges of
-    # an image, which comes from both the down/up rezzing introduced by going through the SDF process, but also
-    # minor differences with IM's SVG rasterizer. What we're trying to catch is full on pixel mismatches that
-    # indicate a shape was mis-handled (curve shooting off to the edge, fill rule violation, etc).
-
     total_pixels = args.render_size * args.render_size
+    (passed, msg) = compare(path, render_path, diff_path, total_pixels, args)
 
-    p = Popen(["compare -fuzz %f%% -metric AE \"%s\" \"%s\" \"%s\"" % (args.fuzz, path, render_path, diff_path)],
-              stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
+    return runner.add_result(passed, path, msg, sdf_path, render_path, diff_path)
+
+
+def test_shapedesc(path, args, runner):
+
+    # Allow either *.shapedesc.txt or *.shapedesc
+    (root, ext) = os.path.splitext(path)
+    if ext == ".txt":
+        (root, ext) = os.path.splitext(root)
+    
+    mode = args.mode
+    sz = args.sdf_size
+    
+    # Ignore specified render size, and match the reference image provided.
+
+    sdf_path = "%s.%s.png" % (root, mode)
+    ref_path = "%s.%s-ref.png" % (root, mode)
+    render_path = "%s.%s-render.png" % (root, mode)
+    diff_path = "%s.%s-diff.png" % (root, mode)
+    
+    p = Popen(["identify \"%s\"" % ref_path], stdin=PIPE, stdout=PIPE, shell=True)
     output, err = p.communicate("")
-    passed = False
-    if p.returncode == 1:
-        # Ran successfully, but they are different. Let's parse the output and check the actual error metric.
-        match = re.match(r'^(\d+)$', err)
-        passed = False
-        if match:
-            try:
-                pixels = int(match.group(1))
-                e = float(pixels) / total_pixels * 100.0
-                if e > args.fail_threshold:
-                    msg = "Error = %.3f %% (%d)" % (e, pixels)
-                else:
-                    passed = True
-                    msg = "OK = %.3f %% (%d)" % (e, pixels)
-            except ValueError:
-                msg = "(Unknown) Error metric = %s" % err
-        else:
-            msg = "(Unknown) Error metric = %s" % err
-    elif p.returncode != 0:
-        msg = "Error comparing to %s [%d]: %s" % (render_path, p.returncode, err)
-    else:
-        passed = True
-        msg = "Identical"
+    if p.returncode != 0:
+        return runner.add_result(False, path, "Error identifying reference image %s: %s" % (ref_path, err))
 
+    match = re.match(r'.+\s\S+\s(\d+)x(\d+)\s', output)
+    if not match:
+        return runner.add_result(False, path, "Could not determine size of reference image")
+
+    rw = match.group(1)
+    rh = match.group(2)
+    
+    try:
+        result = call([args.exe, mode, '-shapedesc', path, '-o', sdf_path,
+                        '-autoframe',
+                        '-testrender', render_path, rw, rh], shell=False)
+        if result != 0:
+            return runner.add_fail(path, "Unable to render %s" % mode)
+    except OSError as er:
+        return runner.add_fail(path, "Error running %s: %s" % (args.exe, er))
+
+    total_pixels = int(rw) * int(rh)
+    (passed, msg) = compare(ref_path, render_path, diff_path, total_pixels, args)
     return runner.add_result(passed, path, msg, sdf_path, render_path, diff_path)
 
 
@@ -137,6 +185,9 @@ def main():
                         help="Directory to scan for SVG files.")
     parser.add_argument("--svg",
                         help="SVG file to test",
+                        action='append')
+    parser.add_argument("--shapedesc",
+                        help="Shapedesc file to test",
                         action='append')
     parser.add_argument("--mode",
                         help="Algorithm: [sdf, psdf, msdf] (default=msdf)",
@@ -153,7 +204,7 @@ def main():
                         help="Path to MSDFGEN executable",
                         default="msdfgen")
     parser.add_argument("--fail-threshold",
-                        help="Percentage of pixels that are allowed to be different without flagging a failure. (Default: 0.06%)",
+                        help="Percentage of pixels that are allowed to be different without flagging a failure. (Default: 0.06%%)",
                         default=0.06,  # Found experimentally (~150.0 / (512 * 512))
                         type=float)
     parser.add_argument("--fuzz",
@@ -174,7 +225,7 @@ def main():
 
     args = parser.parse_args()
 
-    if not args.svg_dir and not args.svg:
+    if not args.svg_dir and not args.svg and not args.shapedesc:
         parser.print_help()
         sys.exit(1)
 
@@ -198,6 +249,16 @@ def main():
                     test_svg(line.rstrip('\n'), args, runner)
             else:
                 test_svg(file, args, runner)
+    
+    if args.shapedesc:
+        for file in args.shapedesc:
+            if file.startswith('@'):
+                # Use ImageMagick style @filename.txt for loading filenames to test.
+                for line in codecs.open(file[1:], 'r', 'utf-8'):
+                    test_shapedesc(line.rstrip('\n'), args, runner)
+            else:
+                test_shapedesc(file, args, runner)
+    
 
     if args.montage:
         # We keep the diff montage at actual size (we want to see details)
